134f37bb81c8af0b54c5560b116d480e512b7a04
Maxim <95439259+MaxLS11@users.noreply.github.com>
2023-05-13 17:48:16 +0200

    Update Fuseit.sol

--- Smartcontract/Fuseit.sol
+++ Smartcontract/Fuseit.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FSNNFT is ERC721, ERC721Burnable, Ownable {
+    constructor() ERC721("FSNNFT", "FSN") {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
 
